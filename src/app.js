2b4743cc917ff986a697d63dd05691bf95535da7
Sansar Kakkar <sansarkakkar07@gmail.com>
2026-05-19 23:46:24 +0530

    chat dis error

--- src/app.js
+++ src/app.js
@@ -16,11 +16,13 @@ const {authRouter}=require("./routes/auth");
 const {profileRouter}=require("./routes/profile");
 const {requestRouter}=require("./routes/request");
 const userRouter=require("./routes/user");
+const chatRouter=require("./routes/chat");
 const intializeSocket = require('./utils/socket');
 app.use('/',authRouter);
 app.use('/',profileRouter);
 app.use('/',requestRouter);
 app.use('/',userRouter);
+app.use('/',chatRouter);
 
 const server=http.createServer(app);
 intializeSocket(server);
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,27 +1,53 @@
-const socket=require("socket.io");
-const intializeSocket=(server)=>{
+const socket = require("socket.io");
+const {Chat } = require("../models/chat");
 
-const io=socket(server,{
-    cors:{
-        origin:"http://localhost:5173",}
-});
-io.on("connection",(socket)=>{
-    socket.on("joinChat",({firstName,userId,targetUserId})=>{
-        const roomId=[userId,targetUserId].sort().join("_");
-        console.log(firstName+"joined room",roomId);
-        socket.join(roomId);
-    });
-    socket.on("sendMessage",({ firstName,
-        userId,
-        targetUserId,
-        text,})=>{
-            const roomId=[userId,targetUserId].sort().join("_");
-            console.log(text);
-            io.to(roomId).emit("messageRecieved",{firstName,text});
-    });
-    socket.on("disconnect",()=>{
+const intializeSocket = (server) => {
 
+  const io = socket(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true,
+    }
+  });
+
+  io.on("connection", (socket) => {
+
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const roomId = [userId, targetUserId].sort().join("_");
+      console.log(firstName + " joined room", roomId);
+      socket.join(roomId);
     });
-});
+
+    socket.on("sendMessage", async ({ firstName, userId, targetUserId, text }) => {
+      const roomId = [userId, targetUserId].sort().join("_");
+
+      try {
+        let chatDoc = await Chat.findOne({
+          participants: { $all: [userId, targetUserId] }
+        });
+
+        if (!chatDoc) {
+          chatDoc = new Chat({
+            participants: [userId, targetUserId],
+            messages: [],
+          });
+        }
+
+        chatDoc.messages.push({
+          senderId: userId,
+          text,
+        });
+
+        await chatDoc.save();
+
+        io.to(roomId).emit("messageRecieved", { firstName, text });
+
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+  });
 };
-module.exports=intializeSocket;
+
+module.exports = intializeSocket;
